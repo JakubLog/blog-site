afaffa5a8d4833f5e6e920241918681deab80fe5
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-23 05:29:24 +0100

    Fix types in BlogID component

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,26 +1,30 @@
 import axios from 'axios';
-import { NextPage } from 'next';
+import { GetStaticProps, GetStaticPaths, NextPage } from 'next';
 import React, { useEffect } from 'react';
 
 interface props {
-  article: any;
+  article: {
+    id: string;
+    title: string;
+    content: string;
+  };
 }
 
 const BlogID: NextPage<props> = ({ article }) => {
   useEffect(() => {
     document.title = `Jakub Michał Fedoszczak | ${article.title}`;
-  }, []);
+  }, [article.title]);
 
   return <div>Post number {article.id}</div>;
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const allArticles = await axios.post(
     'https://api.m3o.com/v1/db/Read',
     { table: 'articles' },
     { headers: { Authorization: `Bearer ${process.env.NEXT_APP_DB_API_KEY}` } }
   );
-  const paths = allArticles.data.records.map((article: any) => {
+  const paths = allArticles.data.records.map((article: props['article']) => {
     return {
       params: {
         id: article.id.toString()
@@ -34,16 +38,22 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async ({ params }: any) => {
-  const article = await axios.post(
-    'https://api.m3o.com/v1/db/Read',
-    { table: 'articles', id: params.id },
-    { headers: { Authorization: `Bearer ${process.env.NEXT_APP_DB_API_KEY}` } }
-  );
-
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (params) {
+    const article = await axios.post(
+      'https://api.m3o.com/v1/db/Read',
+      { table: 'articles', id: params.id },
+      { headers: { Authorization: `Bearer ${process.env.NEXT_APP_DB_API_KEY}` } }
+    );
+    return {
+      props: {
+        article: article.data.records[0]
+      }
+    };
+  }
   return {
     props: {
-      article: article.data.records[0]
+      article: []
     }
   };
 };
