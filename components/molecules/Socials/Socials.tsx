feb1bd60ea4919bac28ff7dca98a684f22035ec8
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-24 18:25:52 +0100

    Small changes in Social component

--- components/molecules/Socials/Socials.tsx
+++ components/molecules/Socials/Socials.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Image from 'next/image';
 import { socials } from 'assets/config/socials';
 import { Wrapper, Social } from './Socials.styles';
+import { nanoid } from 'nanoid';
 
 const Socials = () => {
   return (
     <Wrapper>
       {socials.map(({ name, link, svg }) => (
-        <Social href={link} title={name} role="button" target="_blank" rel="noopener noreferrer">
+        <Social key={nanoid()} aria-label={name} href={link} title={name} role="button" target="_blank" rel="noopener noreferrer">
           <Image width={35} alt={name} height={35} src={svg} />
         </Social>
       ))}
