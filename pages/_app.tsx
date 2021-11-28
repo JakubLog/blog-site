091e78a1470c3fd93266a933d3a0394e9e2ae8e4
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-28 07:04:26 +0100

    Add loading animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,36 @@
 import '../styles/globals.css';
 import '../styles/loading.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import MainTemplate from '../components/templates/MainTemplate';
 import AppProviders from 'providers/AppProviders';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import 'styles/nprogress.css';
+import 'nprogress/nprogress.css';
+
+const handleStartLoading = () => {
+  NProgress.start();
+};
+const handleStopLoading = () => {
+  NProgress.done();
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleStartLoading);
+    router.events.on('routeChangeComplete', handleStopLoading);
+    router.events.on('routeChangeError', handleStopLoading);
+    return () => {
+      router.events.off('routeChangeStart', handleStartLoading);
+      router.events.off('routeChangeComplete', handleStopLoading);
+      router.events.off('routeChangeError', handleStopLoading);
+    };
+  }, [router]);
+
   return (
     <AppProviders>
       <Head>
