6cf879ffba4a78ceb27d4f0715d072ed88f237a0
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-29 18:54:39 +0100

    Fix problem with custom URLs

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -5,6 +5,7 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import React, { useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Content, StyledTitle, Wrapper } from 'styles/Article.styles';
+import Sources from '../../components/organisms/Sources/Sources';
 
 interface props {
   article: {
@@ -12,10 +13,14 @@ interface props {
     title: string;
     content: string;
     category: string;
+    sources?: {
+      name: string;
+      url: string;
+    }[];
   };
 }
 
-const BlogID: NextPage<props> = ({ article: { title, content, category } }) => {
+const BlogID: NextPage<props> = ({ article: { title, content, category, sources } }) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const observer = useRef<any>(null);
   useEffect(() => {
@@ -36,15 +41,105 @@ const BlogID: NextPage<props> = ({ article: { title, content, category } }) => {
       { threshold: 1, root: document }
     );
 
-    getHeaders().forEach((header: Element) => {
-      observer.current.observe(header);
-    });
+    setTimeout(() => {
+      getHeaders().forEach((header: Element) => {
+        observer.current.observe(header);
+      });
+    }, 2000);
 
     return () => {
       observer.current.disconnect();
     };
   }, []);
-  
+
+  content = `# Tytul numer 1
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+  minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+  quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+  fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+  consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+  doloremque. Quaerat provident commodi consectetur veniam similique ad 
+  earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+  fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+  suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+  modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+  totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+  quasi aliquam eligendi, placeat qui corporis!
+  ## Tytul numer 2
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+  minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+  quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+  fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+  consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+  doloremque. Quaerat provident commodi consectetur veniam similique ad 
+  earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+  fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+  suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+  modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+  totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+  quasi aliquam eligendi, placeat qui corporis!
+  ### Tytul numer 3
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+  minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+  quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+  fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+  consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+  doloremque. Quaerat provident commodi consectetur veniam similique ad 
+  earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+  fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+  suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+  modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+  totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+  quasi aliquam eligendi, placeat qui corporis!
+  #### Tytul numer 4
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+  minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+  quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+  fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+  consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+  doloremque. Quaerat provident commodi consectetur veniam similique ad 
+  earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+  fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+  suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+  modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+  totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+  quasi aliquam eligendi, placeat qui corporis!`;
 
   return (
     <Wrapper>
@@ -52,6 +147,7 @@ const BlogID: NextPage<props> = ({ article: { title, content, category } }) => {
       <Content id='article'>
         <ReactMarkdown>{content}</ReactMarkdown>
       </Content>
+      {sources && <Sources data={sources} />}
     </Wrapper>
   );
 };
@@ -75,7 +171,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: true
   };
 };
-
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   if (params) {
     const article = await axios.post(
