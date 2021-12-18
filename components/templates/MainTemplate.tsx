4bc0a663aa882b1e4ebf66d1c661ec7214d375d3
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-18 05:17:26 +0100

    Edit icons for navigation button

--- components/templates/MainTemplate.tsx
+++ components/templates/MainTemplate.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Bar from 'components/organisms/Bar/Bar';
-import { Site, NavButton } from './MainTemplate.styles';
+import { NavButton, Site } from './MainTemplate.styles';
 import Footer from 'components/molecules/Footer/Footer';
 import { theme } from 'styles/theme';
 
@@ -26,8 +26,41 @@ const MainTemplate: React.FC = ({ children }) => {
         {children}
       </main>
       <NavButton aria-label="Navigation button" isOpen={isOpen} onClick={toggleState}>
-        <span>&gt;</span>
-        <span>X</span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-menu"
+          >
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-x"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </span>
       </NavButton>
       <Footer />
     </Site>
