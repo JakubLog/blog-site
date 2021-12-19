245ce7dfacc8d156e2fb83f5129345fe27c9394d
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-12-19 05:13:43 +0100

    Exclude styles from about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,39 +1,8 @@
 import React from 'react';
-import { List } from '../components/atoms/List/List';
-
-import styled from 'styled-components';
-import Section from '../components/molecules/Section/Section';
 import Link from 'next/link';
-
-export const Wrapper = styled.div`
-  padding: 90px 25px 30px;
-  max-height: 100%;
-  overflow-y: auto;
-
-  h1 {
-    margin: 0;
-    letter-spacing: 1px;
-    position: relative;
-    width: fit-content;
-    z-index: 2;
-
-    &::after {
-      content: '';
-      position: absolute;
-      width: 102%;
-      z-index: -1;
-      height: 40%;
-      bottom: -4px;
-      left: 50%;
-      transform: translateX(-50%);
-      background-color: ${({ theme }) => theme.colors.secondary};
-    }
-  }
-
-  @media (min-width: ${({ theme }) => theme.sizing.mobile}) {
-    padding: 30px;
-  }
-`;
+import { List } from '../components/atoms/List/List';
+import { Wrapper } from 'styles/About.styles';
+import Section from '../components/molecules/Section/Section';
 
 const about = () => {
   return (
